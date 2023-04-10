72536dc5714d71dcd78de3018c0aa99eff87cf77
Yash Kadam <yash11398@gmail.com>
2023-04-10 17:12:30 +0530

    REDUX DEVTOOLS TS CODE COMMENTED

--- src/main.tsx
+++ src/main.tsx
@@ -7,8 +7,8 @@ import reducerGrp from './reducers'
 import { Provider } from 'react-redux'
 
 const store = createStore(
-  reducerGrp,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducerGrp
+  // , window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   );
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
